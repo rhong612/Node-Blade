d0cc465338a57b97e39ec3f6c001ea73310377d6
Raymond Hong <rhong612@gmail.com>
2018-02-24 14:05:33 -0800

    Got communication working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app); //Creates a server and passes in app as the request handler
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
@@ -11,3 +12,15 @@ http.listen(3000, function() {
 }); //Listen on port 3000
 
 app.use('/client',express.static(__dirname + '/client'));
+
+io.on('connection', function(socket) {
+	console.log("A user has connected.");
+
+	socket.on('disconnect', function() {
+		console.log('A user has disconnected.');
+	});
+
+	socket.on('msg', function(msg) {
+			console.log('message: ' + msg);
+	});
+});
