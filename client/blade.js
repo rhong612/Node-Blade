889a49def6fa6f8029f4d741ac04d365ae7bfafc
Raymond Hong <rhong612@gmail.com>
2018-02-27 22:00:22 -0800

    Refactoring

--- client/blade.js
+++ client/blade.js
@@ -11,36 +11,49 @@ const FIVE_CARD = 'SHOTGUN';
 const SIX_CARD = 'SPEAR';
 const SEVEN_CARD = 'BROADSWORD';
 
+//Other assets
+const AI_BUTTON = 'AI_BUTTON';
+
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 500;
 
 var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'game', { preload: preload, create: create });
 
+//game.state.add('menu', menuState);
+
 
 
 function preload() {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.load.image(BACK, 'images/back.png');
-    this.load.image(BOLT, 'images/bolt.png');
-    this.load.image(BLAST, 'images/blast.png');
-    this.load.image(MIRROR, 'images/mirror.png');
-    this.load.image(FORCE, 'images/force.png');
-    this.load.image(TWO_CARD, 'images/blade_pistols.png');
-    this.load.image(THREE_CARD, 'images/bow.png');
-    this.load.image(FOUR_CARD, 'images/sword.png');
-    this.load.image(FIVE_CARD, 'images/shotgun.png');
-    this.load.image(SIX_CARD, 'images/spear.png');
-    this.load.image(SEVEN_CARD, 'images/broadsword.png');
+
+    //Cards
+    this.load.image(BACK, 'images/cards/back.png');
+    this.load.image(BOLT, 'images/cards/bolt.png');
+    this.load.image(BLAST, 'images/cards/blast.png');
+    this.load.image(MIRROR, 'images/cards/mirror.png');
+    this.load.image(FORCE, 'images/cards/force.png');
+    this.load.image(TWO_CARD, 'images/cards/blade_pistols.png');
+    this.load.image(THREE_CARD, 'images/cards/bow.png');
+    this.load.image(FOUR_CARD, 'images/cards/sword.png');
+    this.load.image(FIVE_CARD, 'images/cards/shotgun.png');
+    this.load.image(SIX_CARD, 'images/cards/spear.png');
+    this.load.image(SEVEN_CARD, 'images/cards/broadsword.png');
+
+    //Other assets
+    this.load.image(AI_BUTTON, 'images/ai_button.png');
 }
 
 function create() {
-    /*
-    var image = this.add.image(0, 0, BOLT);
+    var image = this.add.image(0, 0, AI_BUTTON);
     image.inputEnabled = true;
-    image.events.onInputDown.add(listener);
-    */
+    image.events.onInputDown.add(versusAI);
+
     this.stage.backgroundColor = "#4488AA";
     var text = this.add.text(game.world.centerX, game.world.centerY, 'Blade', { fontSize: '100px' });
     text.anchor.setTo(0.5);
+}
+
+function versusAI() {
+    console.log("Starting game against AI...");
 }
