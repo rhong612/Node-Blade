2c02fc0984b62db8aee2eee6f662690e8a109133
Raymond Hong <rhong612@gmail.com>
2018-02-28 21:14:46 -0800

    card animation (:

--- client/blade.js
+++ client/blade.js
@@ -3,6 +3,7 @@ const GAME_HEIGHT = 600;
 
 var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'game');
 
+
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 game.state.add('single_play', singlePlayState);
--- client/states/single_play.js
+++ client/states/single_play.js
@@ -1,15 +1,35 @@
 
-const cardWidth = 191; //191 pixel images
+const cardWidth = 191;
 const cardScale = 0.5;
+
+var backSprites = [];
+
+var handSprites = [];
+var hand = [];
 
 var singlePlayState = {
 	create: function() {
 		socket.emit('start_single_game');
 
 		socket.on('receive_hand', function(cards) {
+			/* Draw hand
 			for (let i = 0; i < cards.length; i++) {
-				game.add.image(i * cardWidth * cardScale, 0, cards[i]).scale.setTo(cardScale, cardScale);
+				handSprites.push(game.add.sprite(i * cardWidth * cardScale, 0, cards[i]));
+				handSprites[i].scale.setTo(cardScale, cardScale);
 			}
+			*/
+			hand = cards.slice();
+			let tweens = [];
+			for (let i = 0; i < cards.length; i++) {
+				backSprites.push(game.add.sprite(-1 * cardWidth, 0, BACK));
+				backSprites[i].scale.setTo(cardScale, cardScale);
+				tweens.push(game.add.tween(backSprites[i]).to({ x: game.world.centerX / 20 }, 300, Phaser.Easing.Linear.Out, false, i * 100));
+			}
+			for(let i = 0; i < tweens.length; i++) {
+				tweens[i].start();
+			}
+
+
 		});
 	},
 	update: function() {
