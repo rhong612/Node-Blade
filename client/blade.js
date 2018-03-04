46eeb52e1cc947981e4c9bc8ccf28b12dd5f75f0
Raymond Hong <rhong612@gmail.com>
2018-03-04 13:11:55 -0800

    Added sort-flip animation. WIP

--- client/blade.js
+++ client/blade.js
@@ -65,15 +65,66 @@ function playSetupAnimation() {
 					        for (let h = 0; h < INITIAL_HAND_SIZE - 1; h++) {
 					        	getFlipTween(playerHandSprites.getChildAt(h), hand[h], h * DELAY).start();
 					        }
-					        let lastFlip = getFlipTween(playerHandSprites.getChildAt(INITIAL_HAND_SIZE - 1), hand[INITIAL_HAND_SIZE - 1], (INITIAL_HAND_SIZE - 1) * DELAY);
-					        lastFlip.onComplete.add(function() {
-					            let drawAnim = getDrawAnimationChain();
-					            drawAnim.playerTween.start();
-					            drawAnim.enemyTween.start();
+							const FLIP_SPEED = 200;
+							const delay = (INITIAL_HAND_SIZE - 1) * DELAY;
+						    let flipTween = game.add.tween(playerHandSprites.getChildAt(INITIAL_HAND_SIZE - 1).scale).to({
+						        x: 0,
+						        y: CARD_SCALE * 1.2 //Expand y to give the flip a slight "3d" effect
+						    }, FLIP_SPEED, Phaser.Easing.Linear.None, false, delay);
+
+						    let flipTween2 = game.add.tween(playerHandSprites.getChildAt(INITIAL_HAND_SIZE - 1).scale).to({
+						        x: CARD_SCALE,
+						        y: CARD_SCALE
+						    }, FLIP_SPEED, Phaser.Easing.Linear.None, false);
+
+						    flipTween.onComplete.add(function() {
+						        playerHandSprites.getChildAt(INITIAL_HAND_SIZE - 1).loadTexture(hand[INITIAL_HAND_SIZE - 1]);
+						        flipTween2.start();
+						    });
+					        flipTween2.onComplete.add(function() {
+					        	game.world.bringToTop(playerDeckSprites);
+					        	game.world.bringToTop(enemyDeckSprites);
+
+
+					        	//Sort
+					        	const SORT_DELAY = 500;
+					        	for (let k = 0; k < INITIAL_HAND_SIZE; k++) {
+					        		let sortTween = game.add.tween(playerHandSprites.getChildAt(k)).to({ x: GAME_WIDTH / 2 }, SPEED, Phaser.Easing.Linear.Out, false, SORT_DELAY);
+
+					        		let sprite = playerHandSprites.getChildAt(k);
+					        		let newCard = BACK;
+								    let flipTween = game.add.tween(sprite.scale).to({
+								        x: 0,
+								        y: CARD_SCALE * 1.2 //Expand y to give the flip a slight "3d" effect
+								    }, FLIP_SPEED, Phaser.Easing.Linear.None, false, delay);
+
+								    let flipTween2 = game.add.tween(sprite.scale).to({
+								        x: CARD_SCALE,
+								        y: CARD_SCALE
+								    }, FLIP_SPEED, Phaser.Easing.Linear.None, false);
+
+								    flipTween.onComplete.add(function() {
+								        sprite.loadTexture(newCard);
+								        flipTween2.start();
+								    });
+
+								    flipTween2.onComplete.add(function() {
+								    	getFlipTween(playerHandSprites.getChildAt(k), hand[k], SORT_DELAY / 2).start();
+								    });
+
+					        		
+					        		sortTween.chain(flipTween);
+
+					        		sortTween.start();
+					        	}
+
+
+
+					            //let drawAnim = getDrawAnimationChain();
+					            //drawAnim.playerTween.start();
+					            //drawAnim.enemyTween.start();
 					        });
-					        lastFlip.start();
-					        game.world.bringToTop(playerDeckSprites);
-					        game.world.bringToTop(enemyDeckSprites);
+					        flipTween.start();
 			    		});
 			    	}
 
@@ -104,7 +155,7 @@ function playSetupAnimation() {
 }
 
 function getFlipTween(sprite, newCard, delay) {
-	const FLIP_SPEED = 100;
+	const FLIP_SPEED = 200;
     let flipTween = game.add.tween(sprite.scale).to({
         x: 0,
         y: CARD_SCALE * 1.2 //Expand y to give the flip a slight "3d" effect
@@ -113,11 +164,14 @@ function getFlipTween(sprite, newCard, delay) {
     let flipTween2 = game.add.tween(sprite.scale).to({
         x: CARD_SCALE,
         y: CARD_SCALE
-    }, FLIP_SPEED, Phaser.Easing.Linear.None);
+    }, FLIP_SPEED, Phaser.Easing.Linear.None, false);
 
     flipTween.onComplete.add(function() {
         sprite.loadTexture(newCard);
         flipTween2.start();
     });
     return flipTween;
+}
+
+function cardSort() {
 }
