2b0484ccfd8db3600640e6408695d538663937d9
Raymond Hong <rhong612@gmail.com>
2018-09-04 16:55:31 -0700

    Front end improvements

--- client/states/menu.js
+++ client/states/menu.js
@@ -1,6 +1,30 @@
 var menuState = {
 	create: function() {
-        game.menuBGM.loopFull();
+        //Global speaker icon
+        game.soundSprite = game.make.sprite(0, 0, SOUND_ICON);
+        game.muteSprite = game.make.sprite(0, 0, MUTE_ICON);
+        game.muteSprite.visible = false;
+        game.stage.addChild(game.soundSprite);
+        game.stage.addChild(game.muteSprite);
+
+        game.soundSprite.inputEnabled = true;
+        game.soundSprite.events.onInputDown.add(function() {
+            game.soundSprite.visible = false;
+            game.soundSprite.inputEnabled = false;
+            game.muteSprite.visible = true;
+            game.muteSprite.inputEnabled = true;
+            game.menuBGM.mute = true;
+            game.gameBGM.mute = true;
+        })
+        game.muteSprite.events.onInputDown.add(function() {
+            game.muteSprite.visible = false;
+            game.muteSprite.inputEnabled = false;
+            game.soundSprite.visible = true;
+            game.soundSprite.inputEnabled = true;
+            game.menuBGM.mute = false;
+            game.gameBGM.mute = false;
+        })
+
 
         let image = this.add.image(0, game.world.centerX, MULTI_BUTTON);
         image.inputEnabled = true;
@@ -8,9 +32,5 @@ var menuState = {
             game.state.start('multi_play_menu');
         });
 
-
-        this.stage.backgroundColor = "#4488AA";
-        let text = this.add.text(game.world.centerX, game.world.centerY, 'Blade', { fontSize: '100px' });
-        text.anchor.setTo(0.5);
     }
 }
---
+++ client/states/title.js
@@ -0,0 +1,40 @@
+var titleState = {
+	create: function() {
+        game.menuBGM.loopFull();
+        this.timer = 0;
+        this.stage.backgroundColor = "#4488AA";
+
+        let swords = this.add.image(game.world.centerX, game.world.centerY, CROSSED_SWORDS);
+        swords.alpha = 0;
+        swords.anchor.setTo(0.5);
+        swords.scale.setTo(0.75);
+
+        this.clickText = this.add.text(game.world.centerX, GAME_HEIGHT * 3/4, 'Click anywhere to start', {fontSize: '30px'});
+        this.clickText.alpha = 0;
+        this.clickText.anchor.setTo(0.5);
+
+        let text = this.add.text(game.world.centerX, game.world.centerY, 'Blade', { fontSize: '120px' });
+        text.alpha = 0;
+        text.anchor.setTo(0.5);
+
+        const FADE_IN_SPEED = 2000;
+        this.add.tween(swords).to({alpha: 1}, FADE_IN_SPEED, Phaser.Easing.Linear.None, true);
+        this.add.tween(text).to({alpha: 1}, FADE_IN_SPEED, Phaser.Easing.Linear.None, true);
+    },
+    update: function() {
+        this.timer += game.time.elapsed;
+        if (this.timer >= 1000) {
+            this.timer = 0;
+            if (this.clickText.alpha === 1) {
+                this.clickText.alpha = 0;
+            }
+            else {
+                this.clickText.alpha = 1;
+            }
+        }
+
+        if (game.input.activePointer.isDown) {
+            game.state.start('menu');
+        }
+    }
+}
